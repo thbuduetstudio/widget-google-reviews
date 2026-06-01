5f39c22414fadd661d2b7dc3e52bda9230feeae8
Thierry Burrig <thierry@duet-studio.com>
2026-06-01 15:53:47 +0200

    Create reviews.js

---
+++ api/reviews.js
@@ -0,0 +1,42 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  const { place_id } = req.query;
+
+  if (!place_id) {
+    return res.status(400).json({ error: "Missing place_id" });
+  }
+
+  try {
+    const response = await fetch(
+      `https://places.googleapis.com/v1/places/${place_id}?languageCode=fr`,
+      {
+        headers: {
+          "X-Goog-Api-Key": process.env.GOOGLE_API_KEY,
+          "X-Goog-FieldMask":
+            "displayName,rating,userRatingCount,reviews,googleMapsUri"
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return res.status(response.status).json(data);
+    }
+
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=86400, stale-while-revalidate=604800"
+    );
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      error: "Unable to fetch Google reviews"
+    });
+  }
+}
