6e81739fb44f13ea3e4a775f5d2280fa111f997a
Thierry Burrig <thierry@duet-studio.com>
2026-06-01 16:47:49 +0200

    Update find-place.js

--- api/find-place.js
+++ api/find-place.js
@@ -1,33 +1,32 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const { q } = req.query;
+  const { q, location } = req.query;
 
-  if (!q) {
-    return res.status(400).json({ error: "Missing query" });
-  }
+  if (!q) return res.status(400).json({ error: "Missing query" });
 
   try {
+    const textQuery = location ? `${q} ${location}` : q;
+
     const response = await fetch("https://places.googleapis.com/v1/places:searchText", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "X-Goog-Api-Key": process.env.GOOGLE_API_KEY,
         "X-Goog-FieldMask":
-          "places.id,places.displayName,places.formattedAddress,places.rating,places.userRatingCount"
+          "places.id,places.displayName,places.formattedAddress,places.rating,places.userRatingCount,places.googleMapsUri"
       },
       body: JSON.stringify({
-        textQuery: q,
+        textQuery,
         languageCode: "fr",
-        maxResultCount: 5
+        regionCode: "FR",
+        maxResultCount: 10
       })
     });
 
     const data = await response.json();
 
-    if (!response.ok) {
-      return res.status(response.status).json(data);
-    }
+    if (!response.ok) return res.status(response.status).json(data);
 
     return res.status(200).json(data.places || []);
   } catch (error) {
