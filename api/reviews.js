a8560fe9b592e78af467804d2032ac6fc077405d
Thierry Burrig <thierry@duet-studio.com>
2026-06-12 13:16:49 +0200

    Update reviews.js

--- api/reviews.js
+++ api/reviews.js
@@ -1,5 +1,8 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
 
   const { place_id } = req.query;
 
@@ -14,7 +17,7 @@ export default async function handler(req, res) {
         headers: {
           "X-Goog-Api-Key": process.env.GOOGLE_API_KEY,
           "X-Goog-FieldMask":
-            "id,displayName,rating,userRatingCount,reviews,googleMapsUri"
+            "id,displayName,rating,userRatingCount,reviews,googleMapsUri,websiteUri"
         }
       }
     );
@@ -25,10 +28,15 @@ export default async function handler(req, res) {
       return res.status(response.status).json(data);
     }
 
-    res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate=604800");
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=86400, stale-while-revalidate=604800"
+    );
 
     return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({ error: "Unable to fetch Google reviews" });
+    return res.status(500).json({
+      error: "Unable to fetch Google reviews"
+    });
   }
 }
